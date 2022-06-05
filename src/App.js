4bb42449003f5516ff322b5b78163e1b5e4ecb33
unknown <faatiehsaaqib366@gmail.com>
2022-06-05 20:52:38 +0200

    payment form changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Products, Navbar, Cart } from "./components";
+import { Products, Navbar, Cart, Checkout } from "./components";
 import { commerce } from "./lib/commerce";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 const App = () => {
@@ -16,6 +16,19 @@ const App = () => {
     const item = commerce.cart.add(productId, quantity);
     setCart(item.cart);
   };
+  const handleUpdateCartQty = async (productId, quantity) => {
+    const { cart } = await commerce.cart.update(productId, { quantity });
+    setCart(cart);
+  };
+
+  const handleRemoveFromCart = async (productId) => {
+    const { cart } = await commerce.cart.remove(productId);
+    setCart(cart);
+  };
+  const handleEmptyCart = async () => {
+    const { cart } = await commerce.cart.empty();
+    setCart(cart);
+  };
   useEffect(() => {
     fetchProducts();
     fetchCart();
@@ -23,17 +36,25 @@ const App = () => {
   console.log(cart);
   return (
     <Router>
-    <div>
-      <Navbar totalItems={cart.total_items} />
-      <Switch>
-        <Route exact path='/'>
-      <Products products={products} onAddToCart={handleAddToCart} />
-      </Route>
-      <Route exact path='/cart'>
-      <Cart cart={cart} />
-      </Route>
-      </Switch>
-    </div>
+      <div>
+        <Navbar totalItems={cart.total_items} />
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddToCart} />
+          </Route>
+          <Route exact path="/cart">
+            <Cart
+              cart={cart}
+              handleUpdateCartQty={handleUpdateCartQty}
+              handleRemoveFromCart={handleRemoveFromCart}
+              handleEmptyCart={handleEmptyCart}
+            />
+          </Route>
+          <Route exact path="/checkout">
+            <Checkout />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 };
---
+++ src/components/CheckoutForm/FormInput.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {TextField,Grid} from "@material-ui/core";
+import {useFormContext,Controller} from "react-hook-form";
+
+const FormInput = ({name,label,required}) => {
+  return (
+    <Grid item xs={12} sm={6}>
+        <Controller
+        as={TextField} 
+        conntrol={control}
+        fullWidth
+        name={name}
+        label={label}
+        required={required}/>
+        
+    </Grid>
+  )
+}
+
+export default FormInput;
