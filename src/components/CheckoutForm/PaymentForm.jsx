72f521990c6a7644dd738dc646e2a87048aab89b
unknown <faatiehsaaqib366@gmail.com>
2022-06-06 21:09:19 +0200

    stripe payment method and inline styling fixed

--- src/components/CheckoutForm/PaymentForm.jsx
+++ src/components/CheckoutForm/PaymentForm.jsx
@@ -3,12 +3,28 @@ import { Typography,Button,Divider } from '@material-ui/core';
 import {Elements,CardElement,ElementsConsumer} from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import Review from './Review';
+const stripePromise=loadStripe('...')
 const PaymentForm = ({checkoutToken}) => {
   return (
     <>
     <Review checkoutToken={checkoutToken}/>
+    <Divider/>
+    <Typography variant="h6" gutterBottom style={{margin:'20px 0'}}>
+    Payment method
+    </Typography>
+    <Elements stripe={stripePromise}>
+       <ElementsConsumer>
+           {({elements,stripe})=>(
+         <form>
+             <CardElement/>
+             <br/><br/>
+             div
+         </form>
+           )}
+       </ElementsConsumer>
+    </Elements>
     </>
-  )
+  );
 }
 
 export default PaymentForm
